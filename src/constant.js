38dfc53bb89e54b66891ecb265d613d65f884abd
Seckill13 <30368172+Seckill13@users.noreply.github.com>
2022-10-14 19:20:17 +0800

    Create constant.js

---
+++ src/constant.js
@@ -0,0 +1,186 @@
+export const COMMON_STAFFS_TABLE = "sie_common_staffs";
+export const SIE_PROJECTS_TABLE = "sie_projects";
+export const SIE_DISPATCH_RESULT = "sie_dispatch_result"
+export const SIE_HOLIDAY = "sie_holiday"
+
+
+export const pg2_staffs = [
+    {
+        id:0,
+        priority: 1,
+        name: '刘霞',
+        sex: '女'
+    },
+    {
+        id:1,
+        priority: 2,
+        name: '梁晓萌',
+        sex: '女'
+    },
+    {
+        id:2,
+        priority: 3,
+        name: '谢斌',
+        sex: '男'
+    },
+    {
+        id:3,
+        priority: 4,
+        name: '黄艳丽',
+        sex: '女'
+    },
+    {
+        id:4,
+        priority: 5,
+        name: '杨敏',
+        sex: '女'
+    },
+    {
+        id:5,
+        priority: 6,
+        name: '朱成玉',
+        sex: '女'
+    },
+    {
+        id:6,
+        priority: 7,
+        name: '张真真',
+        sex: '女'
+    },
+    {
+        id:7,
+        priority: 8,
+        name: '刘雅兰',
+        sex: '女'
+    },
+];
+
+export const pg2_projects =
+    [
+        {
+            id:0,
+            priority: 0,
+            projectManager: "陈学良",
+            projectName: 'CES',
+            projectCount: 10
+        },
+        {
+            id:1,
+            priority: 1,
+            projectManager: "韩衡山",
+            projectName: 'WMS',
+            projectCount: 45
+        },
+        {
+            id:2,
+            priority: 2,
+            projectManager: "陈学良",
+            projectName: 'HWMS',
+            projectCount: 6
+        },
+        {
+            id:3,
+            priority: 3,
+            projectManager: "韩衡山",
+            projectName: '运输',
+            projectCount: 3
+                },
+        {
+            id:4,
+            priority: 4,
+            projectManager: "吴斌",
+            projectName: '清关服务化',
+            projectCount: 46
+        },
+        {
+            id:5,
+            priority: 5,
+            projectManager: "颜志勇",
+            projectName: 'AWMS',
+            projectCount: 6
+        },
+        {
+            id:6,
+            priority: 6,
+            projectManager: "陈建超",
+            projectName: 'SCM-站点应用',
+            projectCount: 10
+        },
+        {
+            id:7,
+            priority: 7,
+            projectManager: "韩衡山",
+            projectName: 'DF',
+            projectCount: 37
+        },
+        {
+            id:8,
+            priority: 8,
+            projectManager: "陈学良",
+            projectName: '标签云',
+            projectCount: 3
+        },
+        {
+            id:9,
+            priority: 9,
+            projectManager: "颜志勇",
+            projectName: '计划-存货效率',
+            projectCount: 12
+        },
+        {
+            id:10,
+            priority: 10,
+            projectManager: "陈建超",
+            projectName: 'IPA',
+            projectCount: 23
+        },
+        {
+            id:11,
+            priority: 11,
+            projectManager: "韩衡山",
+            projectName: '计划-采履一站式',
+            projectCount: 12
+        },
+        {
+            id:12,
+            priority: 12,
+            projectManager: "颜志勇",
+            projectName: '计划-ATP',
+            projectCount: 11
+        },
+        {
+            id:13,
+            priority: 13,
+            projectManager: "刘慧",
+            projectName: 'CPP',
+            projectCount: 10
+        },
+        {
+            id:14,
+            priority: 14,
+            projectManager: "史广鸿",
+            projectName: 'ODS',
+            projectCount: 10
+        },
+        {
+            id:15,
+            priority: 15,
+            projectManager: "李建伟",
+            projectName: 'RMS',
+            projectCount: 10
+        },
+        {
+            id:16,
+            priority: 16,
+            projectManager: "李建伟",
+            projectName: 'OFC',
+            projectCount: 3
+        },
+        {
+            id:17,
+            priority: 17,
+            projectManager: "邹鹏",
+            projectName: 'CCM',
+            projectCount: 16
+        },
+    ]; 
